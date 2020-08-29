984d64f67c021a7508e79ea77398f4366b193fd6
Cybernetic1001 <55415301+Cybernetic1001@users.noreply.github.com>
2020-08-29 15:13:48 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@ public class helloworld
     public static void main(final String[] args)
     
     {
-        System.out.println("I'm a straight gangstsa ");
+        System.out.println("Hello World! ");
     }
 }
